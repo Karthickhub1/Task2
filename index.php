f2d88b6ee93950c449277e0ec4399436a94f7bc8
Karthick <81891148+Karthickhub1@users.noreply.github.com>
2021-09-14 05:12:29 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Task2</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body class="b1">
+    <form action="insert.php" method="POST" autocomplete="off">
+        <label>enter your name:</label>
+        <input type="text" name="name">
+        <br>
+        <button>Submit</button>
+
+    </form>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Task2</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body class="b2">
+<div>
+<?php
+ echo "Welcome"  .$_POST ['name'] ."!";
+  ?>
+  </div>
+</body>
+</html>
